eff77c8a2d908534761ea751a81811454eb6895e
Michelle Jordan <tmsmith1150@live.com>
2023-07-25 17:36:24 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
@@ -19,6 +20,8 @@ export default function App() {
         <Route path="/about" element={<About />}>
         </Route>
         <Route path="/contact" element={<Contact />}>
+        </Route>
+        <Route path="/projects" element={<Projects />}>
         </Route>
       </Routes>
     </BrowserRouter>
