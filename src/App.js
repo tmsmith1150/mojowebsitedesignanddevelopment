808a245508b9d393f592936f1424c4627403c4ce
Michelle Jordan <tmsmith1150@live.com>
2023-12-08 22:19:20 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ export default function App() {
       <NavMenu />
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />}>
-        </Route>
         <Route path="/about" element={<About />}>
         </Route>
         <Route path="/contact" element={<Contact />}>
         </Route>
         <Route path="/projects" element={<Projects />}>
+        </Route>
+        <Route path="/" element={<Home />}>
         </Route>
       </Routes>
     </BrowserRouter>
